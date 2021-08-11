12a3b8d0673b191b940cc127d80c0a3bee3a4379
Mauricio Maletta <mauricio_maletta@hotmail.com>
2021-08-11 15:33:23 -0300

    add globalstyles

--- src/styles/GlobalStyles.ts
+++ src/styles/GlobalStyles.ts
@@ -6,8 +6,26 @@ export default createGlobalStyle`
     padding: 0;
     box-sizing: border-box;
 
-    html, body, #root {
-      max-height: 100vh;
-    }
+    color: var(--white);
   }
+
+  html, body, #root {
+      max-height: 100vh;
+      max-width: 100vw;
+
+      width: 100%;
+      height: 100%;
+  }
+
+  *, button, input {
+    border: 0;
+    background: none;
+    font-family: --apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto,
+     Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+
+  html {
+    background: var(--primary);
+  }
+  
 `;
